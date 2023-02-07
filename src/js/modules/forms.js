7a2c4f77f1817b28f78f240a1541fc98943a8e2e
Kirill Sokolov <79161960+klsokolovv@users.noreply.github.com>
2023-02-07 18:39:42 +0300

    Calculator form
    Added functionality to the calculator form

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,13 +1,11 @@
-const forms = () => {
+import checkNumInputs from "./checkNumInputs";
+
+const forms = (state) => {
     const form = document.querySelectorAll('.form'),
-        inputs = document.querySelectorAll('input'),
-        phoneInputs = document.querySelectorAll('input[name="user_phone"]');
-    
-    phoneInputs.forEach(item => {
-        item.addEventListener('input', () => {
-            item.value = item.value.replace(/\D/, '');
-        });
-    });
+        inputs = document.querySelectorAll('input');
+
+    checkNumInputs('input[name = "user.phone"]');
+        
     const message = {
         loading: 'Загрузка...',
         success: 'Спасибо! Мы скоро с вами свяжемся',
@@ -41,6 +39,12 @@ const forms = () => {
 
             const formData = new FormData(item);
 
+            if (item.getAttribute('data-calc') === 'end') {
+                for (let key in state) {
+                    formData.append(key, state[key]);
+                }
+            }
+
             postData('assets/server.php', formData)
             .then(res => {
                 console.log(res);
@@ -57,7 +61,6 @@ const forms = () => {
             });
         });
     });
-
-};
+}
 
 export default forms;
